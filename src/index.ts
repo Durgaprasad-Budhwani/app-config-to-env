0d977f584de06364f7c34b9f2d5808deda8a2259
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2023-08-03 17:18:05 +0930

    🚀 chore(package.json): update dependencies and devDependencies versions ✨ feat(index.ts): refactor getConfiguration function to use getEnvFromAppConfig function 🐛 fix(index.ts): add newline at the end of the file

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,66 @@
 import {
-  AppConfigDataClient,
-  GetLatestConfigurationCommand,
-  StartConfigurationSessionCommand,
+    AppConfigDataClient,
+    GetLatestConfigurationCommand,
+    StartConfigurationSessionCommand,
 } from "@aws-sdk/client-appconfigdata";
 import * as fs from "fs";
-import { parse } from 'yaml'
+import {parse} from 'yaml'
 import * as path from "path";
 
 interface ConfigurationProps {
-  applicationIdentifier: string;
-  environmentIdentifier: string;
-  profileIdentifier: string;
+    applicationIdentifier: string;
+    environmentIdentifier: string;
+    profileIdentifier: string;
 }
 
 export const getConfiguration = async (
-  { applicationIdentifier, environmentIdentifier, profileIdentifier }: ConfigurationProps
+    {applicationIdentifier, environmentIdentifier, profileIdentifier}: ConfigurationProps
 ) => {
-  const client = new AppConfigDataClient({ region: process.env.AWS_REGION });
-  const token = await client.send(
-    new StartConfigurationSessionCommand({
-      ApplicationIdentifier: applicationIdentifier,
-      EnvironmentIdentifier: environmentIdentifier,
-      ConfigurationProfileIdentifier: profileIdentifier,
-    })
-  );
-  const params = {
-    ConfigurationToken: token.InitialConfigurationToken,
-  };
-  const command = new GetLatestConfigurationCommand(params);
-  const response = await client.send(command);
-  // @ts-ignore
-  return new TextDecoder("utf-8").decode(response.Configuration);
+    const client = new AppConfigDataClient({region: process.env.AWS_REGION});
+    const token = await client.send(
+        new StartConfigurationSessionCommand({
+            ApplicationIdentifier: applicationIdentifier,
+            EnvironmentIdentifier: environmentIdentifier,
+            ConfigurationProfileIdentifier: profileIdentifier,
+        })
+    );
+    const params = {
+        ConfigurationToken: token.InitialConfigurationToken,
+    };
+    const command = new GetLatestConfigurationCommand(params);
+    const response = await client.send(command);
+    // @ts-ignore
+    return new TextDecoder("utf-8").decode(response.Configuration);
 };
 
 interface Props extends ConfigurationProps {
-  envPath: string;
+    envPath: string;
 }
 
+export const getEnvFromAppConfig = async (applicationIdentifier: string, environmentIdentifier: string, profileIdentifier: string) => {
+    const config = await getConfiguration({
+        applicationIdentifier,
+        environmentIdentifier,
+        profileIdentifier
+    });
+
+    // Parse the YAML content into an object
+    return parse(config);
+};
+
 const setConfigurationToEnv = async ({
-                                       applicationIdentifier,
-                                       environmentIdentifier,
-                                       profileIdentifier,
-                                       envPath,
-}: Props) => {
-  const config = await getConfiguration({
-    applicationIdentifier,
-    environmentIdentifier,
-    profileIdentifier
-  });
+                                         applicationIdentifier,
+                                         environmentIdentifier,
+                                         profileIdentifier,
+                                         envPath,
+                                     }: Props) => {
+    const yaml = await getEnvFromAppConfig(applicationIdentifier, environmentIdentifier, profileIdentifier);
 
-  // Parse the YAML content into an object
-  const yaml = parse(config);
+    // Convert the YAML object to a string of key-value pairs
+    const envContent = Object.entries(yaml).map(([key, value]) => `${key}=${value}`).join('\n');
 
-  // Convert the YAML object to a string of key-value pairs
-  const envContent = Object.entries(yaml).map(([ key, value ]) => `${ key }=${ value }`).join('\n');
-
-  // Write the env content to a .env file
-  fs.writeFileSync(path.join(envPath), envContent, 'utf8');
+    // Write the env content to a .env file
+    fs.writeFileSync(path.join(envPath), envContent, 'utf8');
 }
 
 export default setConfigurationToEnv;
